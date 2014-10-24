bfd1877392d1e11fe3042791559f1231a0fe2a60
phantichhoang <phantichhoang@f6597a64-05d1-0719-f566-4b59fd94b772>
2014-10-24 13:40:27 +0000

    Demo buoi 3

--- trunk/hoangpt/2014-10-24/Tutor0301_westjourney/src/com/hoangphan/tutor0301_westjourney/Scr1Activity.java
+++ trunk/hoangpt/2014-10-24/Tutor0301_westjourney/src/com/hoangphan/tutor0301_westjourney/Scr1Activity.java
@@ -1,8 +1,10 @@
 package com.hoangphan.tutor0301_westjourney;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class Scr1Activity extends Activity {
 
@@ -11,12 +13,22 @@ public class Scr1Activity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_scr1);
 		Log.d("Screen1", "Screen1 onCreate");
+		
 	}
 
 	@Override
 	protected void onStart() {
 		super.onStart();
 		Log.d("Screen1", "Screen1 onStart");
+		
+		Intent i = getIntent();
+		String hello = i.getStringExtra("hello");
+		int year = i.getIntExtra("year", 0);
+		
+		Log.d("Bundle", hello);
+		Log.d("Bundle", year+"");
+		String text = hello + "\n" + "This year is "+year;
+		Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
 	}
 
 	@Override
