b1007ed626e59a78c88d739220e2202ef071bcb7
phantichhoang <phantichhoang@f6597a64-05d1-0719-f566-4b59fd94b772>
2014-12-02 13:01:31 +0000

    Test 01

--- hoangpt/2014-12-02/Tutor0901_threading/src/com/hoangphan/tutor0901_threading/CountThread.java
+++ hoangpt/2014-12-02/Tutor0901_threading/src/com/hoangphan/tutor0901_threading/CountThread.java
@@ -2,11 +2,29 @@ package com.hoangphan.tutor0901_threading;
 
 public class CountThread extends Thread {
 	
+	long count = 0;
+	MainActivity act;
+	
+	public CountThread(MainActivity act) {
+		this.act = act;
+	}
+
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		while (true) {
+			try {
+				Thread.sleep(100);
+				count++;
+				act.runOnUiThread(new Runnable() {
+					
+					@Override
+					public void run() {
+						act.txtCount.setText(""+count);
+					}
+				});
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
-	
-
 }
