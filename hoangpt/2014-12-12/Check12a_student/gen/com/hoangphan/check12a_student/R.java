ab7ad71e5ce363af9d9368772d75826ebb5a6536
phantichhoang <phantichhoang@f6597a64-05d1-0719-f566-4b59fd94b772>
2014-12-22 03:46:00 +0000

    Tutor13

--- hoangpt/2014-12-12/Check12a_student/gen/com/hoangphan/check12a_student/R.java
+++ hoangpt/2014-12-12/Check12a_student/gen/com/hoangphan/check12a_student/R.java
@@ -8,17 +8,26 @@
 package com.hoangphan.check12a_student;
 
 public final class R {
+    public static final class array {
+        public static final int menu_items=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int activity_main_content_button_menu=0x7f070001;
+        public static final int activity_main_content_fragment=0x7f070003;
+        public static final int activity_main_content_title=0x7f070002;
+        public static final int activity_main_menu_listview=0x7f070000;
+    }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
     }
     public static final class style {
         /** 
@@ -31,10 +40,10 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- hoangpt/2014-12-12/Check12a_student/src/com/hoangphan/check12a_student/MainActivity.java
+++ hoangpt/2014-12-12/Check12a_student/src/com/hoangphan/check12a_student/MainActivity.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import com.hoangphan.check12a_student.model.Student;
 import com.hoangphan.check12a_student.model.StudentDao;
+import com.hoangphan.check12a_student.ui.MainLayout;
 
 import android.app.Activity;
 import android.app.ListActivity;
@@ -11,14 +12,34 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class MainActivity extends ListActivity {
+	
+    // The MainLayout which will hold both the sliding menu and our main content
+    // Main content will holds our Fragment respectively
+    MainLayout mainLayout;
+    
+    // ListView menu
+    private ListView lvMenu;
+    private String[] lvMenuItems;
+    
+    // Menu button
+    Button btMenu;
+    
+    // Title according to fragment
+    TextView tvTitle;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.activity_main);
 		
 		//logic
 		StudentDao dao = new StudentDao(getBaseContext());
@@ -28,5 +49,47 @@ public class MainActivity extends ListActivity {
 		ArrayList<Student> list = dao.findAll();
 		ArrayAdapter<Student> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
 		setListAdapter(adapter);
+		
+        // Inflate the mainLayout
+        mainLayout = (MainLayout) this.getLayoutInflater().inflate(R.layout.activity_main, null);
+        setContentView(mainLayout);
+        
+        // Init menu
+        lvMenuItems = getResources().getStringArray(R.array.menu_items);
+        lvMenu = (ListView) findViewById(R.id.activity_main_menu_listview);
+        lvMenu.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, lvMenuItems));
+        /*lvMenu.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                onMenuItemClick(parent, view, position, id);
+            }
+            
+        });*/
+        
+        
+        // Get menu button
+        btMenu = (Button) findViewById(R.id.activity_main_content_button_menu);
+        btMenu.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Show/hide the menu
+                toggleMenu(v);
+            }
+        });		
 	}
+	
+    public void toggleMenu(View v){
+        mainLayout.toggleMenu();
+    }
+    
+    @Override
+    public void onBackPressed() {
+        if (mainLayout.isMenuShown()) {
+            mainLayout.toggleMenu();
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
 }
