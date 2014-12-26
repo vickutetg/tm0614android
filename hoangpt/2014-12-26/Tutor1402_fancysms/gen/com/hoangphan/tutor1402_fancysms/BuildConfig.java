aa0f904e0bb4111d135de81d6addfa5b202c355a
phantichhoang <phantichhoang@f6597a64-05d1-0719-f566-4b59fd94b772>
2014-12-26 13:58:07 +0000

    Tutor13

---
+++ hoangpt/2014-12-26/Tutor1402_fancysms/gen/com/hoangphan/tutor1402_fancysms/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.hoangphan.tutor1402_fancysms;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ hoangpt/2014-12-26/Tutor1403_notif/gen/com/hoangphan/tutor1401_notif/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.hoangphan.tutor1401_notif;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
