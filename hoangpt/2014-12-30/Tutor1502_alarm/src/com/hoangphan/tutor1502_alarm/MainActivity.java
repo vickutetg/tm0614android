5ef9dee2b9c71c941788145af09fa69208a970f3
phantichhoang <phantichhoang@f6597a64-05d1-0719-f566-4b59fd94b772>
2014-12-30 14:06:11 +0000

    Tutor13

--- hoangpt/2014-12-30/Tutor1502_alarm/src/com/hoangphan/tutor1502_alarm/MainActivity.java
+++ hoangpt/2014-12-30/Tutor1502_alarm/src/com/hoangphan/tutor1502_alarm/MainActivity.java
@@ -1,11 +1,14 @@
 package com.hoangphan.tutor1502_alarm;
 
+import java.util.Calendar;
+
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,16 +23,19 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		/* Retrieve a PendingIntent that will perform a broadcast */
-        Intent alarmIntent = new Intent(MainActivity.this, AlarmReceiver.class);
-        pendingIntent = (PendingIntent) PendingIntent.getBroadcast(MainActivity.this, 0, alarmIntent, 0);
-
         findViewById(R.id.startAlarm).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 start();
             }
         });
+        
+        findViewById(R.id.stopAlarmAt10).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAt10();
+            }
+        });    
 	}
 	
 	public void start() {
@@ -38,5 +44,28 @@ public class MainActivity extends Activity {
 
         manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), interval, pendingIntent);
         Toast.makeText(this, "Alarm Set", Toast.LENGTH_SHORT).show();
+    }
+	
+	public void startAt10() {
+        AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Toast.makeText(this, "set 10", Toast.LENGTH_LONG).show();
+
+        /* Set the alarm to start at 10:30 AM */
+        /*Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, 20);
+        calendar.set(Calendar.MINUTE, 54);
+
+        manager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                60*1000, pendingIntent);*/
+        
+        long firstTime = SystemClock.elapsedRealtime();
+        firstTime += 1*1000;
+
+        Intent alarmIntent = new Intent(MainActivity.this, AlarmReceiver.class);
+        pendingIntent = (PendingIntent) PendingIntent.getBroadcast(MainActivity.this, 0, alarmIntent, 0);
+        
+        manager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                        firstTime, 2*1000, pendingIntent);
     }
 }
